07cf898c5442230c04bc79cc85b90668004ebedf
Haroen Viaene <hello@haroen.me>
2018-06-07 16:14:54 +0200

    docs(rating): add custom story

--- stories/RatingMenu.stories.js
+++ stories/RatingMenu.stories.js
@@ -18,4 +18,21 @@ storiesOf('RatingMenu', module)
         <ais-rating-menu attribute="rating">
         </ais-rating-menu>
       </div>`,
+  }))
+  .add('custom rendering', () => ({
+    template: `
+      <div>
+        <ais-rating-menu attribute="rating">
+          <template slot-scope="{ items, refine }">
+            rating
+            <ul>
+              <li v-for="(item, key) in items" :key="key">
+                <button @click="refine(item.value)">
+                  {{item.isRefined ? '🙅‍♀️' : '✔️'}} {{item.name}} & up
+                </button>
+              </li>
+            </ul>
+          </template>
+        </ais-rating-menu>
+      </div>`,
   }));
