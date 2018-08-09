72ca85ec682f2ee04fc9f62ac666f7e6afef6e8e
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-08-09 12:33:17 +0200

    chore(Storybook): add styles for preview & playground

--- stories/utils.js
+++ stories/utils.js
@@ -1,6 +1,27 @@
 export const previewWrapper = ({
   indexName = 'instant_search',
-  hits = '',
+  hits = `
+    <ol
+      slot-scope="{ items }"
+      class="playground-hits"
+    >
+      <li
+        v-for="item in items"
+        :key="item.objectID"
+        class="playground-hits-item"
+      >
+        <div
+          class="playground-hits-image"
+          :style="{ backgroundImage: 'url(' + item.image + ')' }"
+        />
+        <div class="playground-hits-desc">
+          <p>{{ item.name }}</p>
+          <p>Rating: {{ item.rating }}✭</p>
+          <p>Price: {{ item.price }}$</p>
+        </div>
+      </li>
+    </ol>
+  `,
 } = {}) => () => ({
   template: `
     <ais-index
@@ -8,14 +29,18 @@ export const previewWrapper = ({
       apiKey="6be0576ff61c053d5f9a3225e2a90f76"
       indexName="${indexName}"
     >
-      <h2>Display</h2>
-      <story />
+      <ais-configure :hitsPerPage="3" />
+      <div class="container container-preview">
+        <story />
+      </div>
 
-      <h2>Hits</h2>
-      <ais-search-box />
-      <ais-hits>
-        ${hits}
-      </ais-hits>
+      <div class="container container-playground">
+        <ais-search-box />
+        <ais-hits>
+          ${hits}
+        </ais-hits>
+        <ais-pagination />
+      </div>
     </ais-index>
   `,
 });
