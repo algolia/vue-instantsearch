e2b9653353f9a8694df4a16473aa2bcea12ccf57
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-08-31 12:14:38 +0200

    wip(Hits)

--- examples/ecommerce/src/App.vue
+++ examples/ecommerce/src/App.vue
@@ -12,13 +12,34 @@
         <ais-search-box placeholder="Search a product" />
       </header>
       <ais-hits>
-        <main slot="default" slot-scope="{ items }">
+        <main
+          slot="default"
+          slot-scope="{ items }"
+          class="products"
+        >
+        <div>
           <article
             v-for="item in items"
             :key="item.objectID"
+            class="product"
           >
-            <ais-highlight attribute="name" :hit="item" />
+            <div class="product-picture-wrapper">
+              <img class="product-picture" :src="item.image" :alt="item.name" />
+            </div>
+            <div class="product-desc-wrapper">
+              <div class="product-name">
+                <ais-highlight attribute="name" :hit="item" />
+              </div>
+              <div class="product-type">
+                <ais-highlight attribute="type" :hit="item" />
+              </div>
+              <div class="product-footer">
+                <!-- Add the stars -->
+                <div class="product-price">${{ item.price }}</div>
+              </div>
+            </div>
           </article>
+          </div>
         </main>
       </ais-hits>
     </ais-index>
@@ -47,6 +68,13 @@ export default {
 </script>
 
 <style>
+* {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -ms-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
 html,
 body {
   margin: 0;
@@ -107,5 +135,61 @@ body {
 
 .ais-SearchBox-reset {
   display: none;
+}
+
+/*
+ * Hits
+ */
+
+.products {
+  margin: 10px -0.5% 0;
+  padding: 0 10px;
+}
+
+.product {
+  width: 24%;
+  float: left;
+  padding: 10px 20px 20px;
+  margin-bottom: 10px;
+  border-bottom: solid 1px #eee;
+  margin: 0.5%;
+  border: solid 1px #eee;
+  box-shadow: 0 0 3px #f6f6f6;
+  position: relative;
+}
+
+.product-picture-wrapper {
+  text-align: center;
+}
+
+.product-picture {
+  height: 150px;
+  max-width: 100%;
+}
+
+.product-desc-wrapper {
+  height: 100px;
+  width: 100%;
+  overflow: hidden;
+}
+
+.product-name {
+  font-weight: bold;
+  color: #000000;
+  font-size: 0.9em;
+  margin: 0 0 8px;
+  min-width: 120px;
+}
+
+.product-type {
+  font-size: 0.8em;
+  margin: 0 0 10px;
+  color: #a2a2a2;
+}
+
+.product-price {
+  font-weight: bold;
+  color: #000000;
+  float: right;
 }
 </style>
