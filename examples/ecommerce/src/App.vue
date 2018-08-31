24afc96625b7c690afdd9da013cda147ce8c3fe5
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-08-31 10:14:42 +0200

    feat(ecommerce): add header

--- examples/ecommerce/src/App.vue
+++ examples/ecommerce/src/App.vue
@@ -6,7 +6,11 @@
       indexName="ikea"
     >
       <ais-configure :hitsPerPage="16" />
-      <ais-search-box />
+      <header class="navbar">
+        <img src="https://res.cloudinary.com/hilnmyskv/image/upload/w_100,h_100,dpr_2.0//v1461180087/logo-instantsearchjs-avatar.png" width="40">
+        <h1 class="navbar-title">aeki</h1>
+        <ais-search-box placeholder="Search a product" />
+      </header>
       <ais-hits>
         <main slot="default" slot-scope="{ items }">
           <article
@@ -22,7 +26,13 @@
 </template>
 
 <script>
-import { Index, Configure, SearchBox, Hits, Highlight } from 'vue-instantsearch';
+import {
+  Index,
+  Configure,
+  SearchBox,
+  Hits,
+  Highlight,
+} from 'vue-instantsearch';
 
 export default {
   name: 'App',
@@ -32,8 +42,8 @@ export default {
     AisSearchBox: SearchBox,
     AisHits: Hits,
     AisHighlight: Highlight,
-  }
-}
+  },
+};
 </script>
 
 <style>
@@ -41,5 +51,61 @@ html,
 body {
   margin: 0;
   padding: 0;
+  font-size: 14px;
+  font-family: Verdana, sans-serif;
+}
+
+.navbar {
+  background-color: #222f3f;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+}
+
+.navbar-title {
+  font-size: 30px;
+  font-weight: bold;
+  color: #ffffff;
+  margin: 0 15px;
+}
+
+/*
+ * SearchBox
+ */
+
+.ais-SearchBox-form {
+  display: flex;
+}
+
+.ais-SearchBox-input {
+  width: 275px;
+  padding: 5px 12px;
+  border: 1px solid transparent;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  font-family: Verdana, sans-serif;
+  font-size: 14px;
+}
+
+.ais-SearchBox-input:focus {
+  border-color: #ffbe61;
+  outline: none;
+}
+
+.ais-SearchBox-submit {
+  background-color: #ffbe61;
+  padding: 0 30px;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  border: none;
+}
+
+.ais-SearchBox-submitIcon {
+  width: 15px;
+  height: 15px;
+}
+
+.ais-SearchBox-reset {
+  display: none;
 }
 </style>
---
+++ examples/ecommerce/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="navbar">
+    <img src="https://res.cloudinary.com/hilnmyskv/image/upload/w_100,h_100,dpr_2.0//v1461180087/logo-instantsearchjs-avatar.png" width="40">
+    <h1 class="navbar__title">aeki</h1>
+    <slot />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style>
+.navbar {
+  background-color: #222f3f;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+}
+
+.navbar__title {
+  font-size: 30px;
+  font-weight: bold;
+  color: #ffffff;
+  margin: 0 15px;
+}
+</style>
