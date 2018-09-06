0b2d8910d80107b40d9b6fedb367a63f765c525d
Haroen Viaene <hello@haroen.me>
2018-09-06 14:48:29 -0700

    docs: add useRequestCache

--- stories/MultiIndex.stories.js
+++ stories/MultiIndex.stories.js
@@ -30,7 +30,11 @@ storiesOf('MultiIndex', module).add('simple usage', () => ({
       query: '',
       searchClient: algoliasearch(
         'latency',
-        '6be0576ff61c053d5f9a3225e2a90f76'
+        '6be0576ff61c053d5f9a3225e2a90f76',
+        {
+          // this is necessary, because we call the same refine 2 times?
+          _useRequestCache: true,
+        }
       ),
     };
   },
