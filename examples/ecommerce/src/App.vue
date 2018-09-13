0ef6aa15cfb6d3a47e2d5437785c092d9506369a
Haroen Viaene <hello@haroen.me>
2018-09-13 15:01:14 -0700

    feat: add other widgets & rudimentary styling

--- examples/ecommerce/src/App.vue
+++ examples/ecommerce/src/App.vue
@@ -5,86 +5,157 @@
       index-name="instant_search"
     >
       <ais-configure :hitsPerPage="16" />
-      <header class="navbar">
-        <img src="https://res.cloudinary.com/hilnmyskv/image/upload/w_100,h_100,dpr_2.0//v1461180087/logo-instantsearchjs-avatar.png" width="40">
-        <h1 class="navbar-title">aeki</h1>
-        <ais-search-box placeholder="Search a product" />
-      </header>
-      <ais-hits>
-        <main
-          slot="default"
-          slot-scope="{ items }"
-          class="products"
-        >
-          <ais-panel>
-            <template slot="header">
-              <h5>Rating</h5>
-            </template>
-            <template slot="default">
-              <ais-rating-menu
-                attribute="rating"
-                :max="5"
-              />
-            </template>
-          </ais-panel>
-          <ais-state-results>
-            <template slot-scope="{ query, hits }">
-              <div class="results-wrapper" v-if="hits.length === 0">
-                <div class="no-results">
-                  No results found matching <span class="query">{{query}}</span>
-                </div>
-              </div>
-            </template>
-          </ais-state-results>
-          <div>
-            <article
-              v-for="item in items"
-              :key="item.objectID"
-              class="product"
-            >
-              <div class="product-picture-wrapper">
-                <img class="product-picture" :src="item.image" :alt="item.name" />
-              </div>
-              <div class="product-desc-wrapper">
-                <div class="product-name">
-                  <ais-highlight attribute="name" :hit="item" />
-                </div>
-                <div class="product-type">
-                  <ais-highlight attribute="type" :hit="item" />
-                </div>
-                <div class="product-footer">
-                  <div class="ais-RatingMenu-link">
-                    <svg
-                      v-for="(_,i) in 5"
-                      :key="i"
-                      :class="[
-                        'ais-RatingMenu-starIcon',
-                        i >= item.rating && 'ais-RatingMenu-starIcon--empty'
-                      ]"
-                      aria-hidden="true"
-                      width="24"
-                      height="24"
-                    >
-                      <use
-                        :xlink:href="`#ais-RatingMenu-star${i >= item.rating ? 'Empty' : ''}Symbol`" 
-                      />
-                    </svg>
-                  </div>
-                  <div class="product-price">${{ item.price }}</div>
-                </div>
-              </div>
-            </article>
+      <my-header />
+
+      <div class="content-wrapper">
+        <aside>
+          <ais-clear-refinements
+          />
+            <!-- translations={{
+              reset: 'Clear all filters',
+            }} -->
+
+          <section class="facet-wrapper">
+            <div class="facet-category-title facet">Show results for</div>
+            <ais-hierarchical-menu
+              :attributes="[
+                'hierarchicalCategories.lvl0',
+                'hierarchicalCategories.lvl1',
+                'hierarchicalCategories.lvl2',
+              ]"
+            />
+          </section>
+
+          <section class="facet-wrapper">
+            <div class="facet-category-title facet">Refine By</div>
+
+            <ais-panel>
+              <template slot="header">
+                <h5>Type</h5>
+              </template>
+              <template slot="default">
+                <ais-refinement-list
+                  attribute="type"
+                  operator="or"
+                  :max="5"
+                />
+              </template>
+            </ais-panel>
+
+            <ais-panel>
+              <template slot="header">
+                <h5>Brand</h5>
+              </template>
+              <template slot="default">
+                <ais-refinement-list
+                  searchable
+                  attribute="brand"
+                  operator="or"
+                  :max="5"
+                />
+              </template>
+            </ais-panel>
+
+            <ais-panel>
+              <template slot="header">
+                <h5>Rating</h5>
+              </template>
+              <template slot="default">
+                <ais-rating-menu
+                  attribute="rating"
+                  :max="5"
+                />
+              </template>
+            </ais-panel>
+
+            <ais-panel>
+              <template slot="header">
+                <h5>Price</h5>
+              </template>
+              <template slot="default">
+                <ais-range-input
+                  attribute="price"
+                  :max="5"
+                />
+              </template>
+            </ais-panel>
+
+          </section>
+
+          <div class="thank-you">
+            Data courtesy of <a href="https://developer.bestbuy.com/">Best Buy</a>
           </div>
-        </main>
-      </ais-hits>
+        </aside>
+
+        <ais-hits>
+          <main
+            slot="default"
+            slot-scope="{ items }"
+            class="products"
+          >
+            <ais-state-results>
+              <template slot-scope="{ query, hits }">
+                <div class="results-wrapper" v-if="hits.length === 0">
+                  <div class="no-results">
+                    No results found matching <span class="query">{{query}}</span>
+                  </div>
+                </div>
+              </template>
+            </ais-state-results>
+            <div>
+              <article
+                v-for="item in items"
+                :key="item.objectID"
+                class="product"
+              >
+                <div class="product-picture-wrapper">
+                  <img class="product-picture" :src="item.image" :alt="item.name" />
+                </div>
+                <div class="product-desc-wrapper">
+                  <div class="product-name">
+                    <ais-highlight attribute="name" :hit="item" />
+                  </div>
+                  <div class="product-type">
+                    <ais-highlight attribute="type" :hit="item" />
+                  </div>
+                  <div class="product-footer">
+                    <div class="ais-RatingMenu-link">
+                      <svg
+                        v-for="(_,i) in 5"
+                        :key="i"
+                        :class="[
+                          'ais-RatingMenu-starIcon',
+                          i >= item.rating && 'ais-RatingMenu-starIcon--empty'
+                        ]"
+                        aria-hidden="true"
+                        width="24"
+                        height="24"
+                      >
+                        <use
+                          :xlink:href="`#ais-RatingMenu-star${i >= item.rating ? 'Empty' : ''}Symbol`" 
+                        />
+                      </svg>
+                    </div>
+                    <div class="product-price">${{ item.price }}</div>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </main>
+        </ais-hits>
+      </div>
     </ais-index>
   </div>
 </template>
 
 <script>
 import algoliasearch from 'algoliasearch/lite';
+import MyHeader from './components/Header.vue';
 
 export default {
+  components: {
+    MyHeader,
+  },
   data() {
     return {
       searchClient: algoliasearch('latency','6be0576ff61c053d5f9a3225e2a90f76'),
@@ -124,44 +195,13 @@ body {
   margin: 0 15px;
 }
 
-/*
- * SearchBox
- */
-
-.ais-SearchBox-form {
+.content-wrapper {
   display: flex;
 }
 
-.ais-SearchBox-input {
-  width: 275px;
-  padding: 5px 12px;
-  border: 1px solid transparent;
-  border-top-left-radius: 4px;
-  border-bottom-left-radius: 4px;
-  font-family: Verdana, sans-serif;
-  font-size: 14px;
-}
-
-.ais-SearchBox-input:focus {
-  border-color: #ffbe61;
-  outline: none;
-}
-
-.ais-SearchBox-submit {
-  background-color: #ffbe61;
-  padding: 0 30px;
-  border-top-right-radius: 4px;
-  border-bottom-right-radius: 4px;
-  border: none;
-}
-
-.ais-SearchBox-submitIcon {
-  width: 15px;
-  height: 15px;
-}
-
-.ais-SearchBox-reset {
-  display: none;
+.content-wrapper aside {
+  /* @TODO: size correctly */
+  width: 15em;
 }
 
 /*
--- examples/ecommerce/src/components/Header.vue
+++ examples/ecommerce/src/components/Header.vue
@@ -2,7 +2,7 @@
   <header class="navbar">
     <img src="https://res.cloudinary.com/hilnmyskv/image/upload/w_100,h_100,dpr_2.0//v1461180087/logo-instantsearchjs-avatar.png" width="40">
     <h1 class="navbar__title">aeki</h1>
-    <slot />
+    <ais-search-box placeholder="Search a product" />
   </header>
 </template>
 
@@ -25,5 +25,45 @@ export default {
   font-weight: bold;
   color: #ffffff;
   margin: 0 15px;
+}
+
+/*
+ * SearchBox
+ */
+
+header .ais-SearchBox-form {
+  display: flex;
+}
+
+header .ais-SearchBox-input {
+  width: 275px;
+  padding: 5px 12px;
+  border: 1px solid transparent;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  font-family: Verdana, sans-serif;
+  font-size: 14px;
+}
+
+header .ais-SearchBox-input:focus {
+  border-color: #ffbe61;
+  outline: none;
+}
+
+header .ais-SearchBox-submit {
+  background-color: #ffbe61;
+  padding: 0 30px;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  border: none;
+}
+
+header .ais-SearchBox-submitIcon {
+  width: 15px;
+  height: 15px;
+}
+
+header .ais-SearchBox-reset {
+  display: none;
 }
 </style>
