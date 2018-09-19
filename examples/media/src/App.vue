ae320cc14b3e05e60f25612b0f0b0975fcf206a0
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-09-19 16:23:37 +0200

    feat(media): setup example

---
+++ examples/media/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ais-index
+    :search-client="searchClient"
+    index-name="movies"
+  >
+    <ais-search-box />
+    <ais-hits />
+  </ais-index>
+</template>
+
+<script>
+import algoliasearch from 'algoliasearch/lite';
+import './App.css';
+
+export default {
+  data() {
+    return {
+      searchClient: algoliasearch(
+        'latency',
+        '6be0576ff61c053d5f9a3225e2a90f76'
+      ),
+    };
+  },
+};
+</script>
