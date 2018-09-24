d9ac94c23f6f66869a8c36ff72b1ffa9f17325c6
Haroen Viaene <hello@haroen.me>
2018-09-24 21:17:33 +0200

    chore: update snaps etc.

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,41 +1,40 @@
+import Vue from 'vue';
 import InstantSearch from '../instantsearch';
 
 test('Should register all components when installed', () => {
-  const component = jest.fn();
-  const Vue = { component };
+  Vue.component = jest.fn();
+  Vue.use(InstantSearch);
 
-  InstantSearch.install(Vue);
+  const allInstalledComponents = Vue.component.mock.calls.map(call => call[0]);
 
-  const components = [
-    'ais-autocomplete',
-    'ais-breadcrumb',
-    'ais-clear-refinements',
-    'ais-configure',
-    'ais-current-refinements',
-    'ais-hierarchical-menu',
-    'ais-highlight',
-    'ais-hits-per-page',
-    'ais-hits',
-    'ais-instant-search',
-    'ais-infinite-hits',
-    'ais-menu',
-    'ais-menu-select',
-    'ais-numeric-menu',
-    'ais-pagination',
-    'ais-panel',
-    'ais-powered-by',
-    'ais-range-input',
-    'ais-rating-menu',
-    'ais-refinement-list',
-    'ais-search-box',
-    'ais-search-state',
-    'ais-snippet',
-    'ais-sort-by',
-    'ais-stats',
-    'ais-toggle-refinement',
-  ];
-
-  const allInstalledComponents = component.mock.calls.map(call => call[0]);
-
-  expect(allInstalledComponents).toEqual(components);
+  expect(allInstalledComponents).toMatchInlineSnapshot(`
+Array [
+  "AisAutocomplete",
+  "AisBreadcrumb",
+  "AisClearRefinements",
+  "AisConfigure",
+  "AisCurrentRefinements",
+  "AisHierarchicalMenu",
+  "AisHighlight",
+  "AisHits",
+  "AisHitsPerPage",
+  "AisInstantSearch",
+  "AisInfiniteHits",
+  "AisMenu",
+  "AisMenuSelect",
+  "AisNumericMenu",
+  "AisPagination",
+  "AisPanel",
+  "AisPoweredBy",
+  "AisRangeInput",
+  "AisRatingMenu",
+  "AisRefinementList",
+  "AisSearchState",
+  "AisSearchBox",
+  "AisSnippet",
+  "AisSortBy",
+  "AisStats",
+  "AisToggleRefinement",
+]
+`);
 });
