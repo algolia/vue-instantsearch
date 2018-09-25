514b95d9c629188012fcfb22559920973f630431
Haroen Viaene <hello@haroen.me>
2018-09-25 11:17:21 +0200

    test: remove useless one

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,71 +1,35 @@
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
-import Vue from 'vue';
+import { createLocalVue } from '@vue/test-utils';
 import InstantSearch from '../instantsearch';
 
-describe('installing the plugin', () => {
+it('should have `name` the same as the suit class name everywhere', () => {
+  const Vue = createLocalVue();
   Vue.component = jest.fn();
   Vue.use(InstantSearch);
 
   const allInstalledComponents = Vue.component.mock.calls;
+  const components = allInstalledComponents.map(
+    ([installedName, { name, mixins }]) => {
+      let suitClass = `Error! ${name} is missing the suit classes`;
 
-  it('should register all components when installed', () => {
-    expect(allInstalledComponents.map(([name, _component]) => name))
-      .toMatchInlineSnapshot(`
-Array [
-  "AisAutocomplete",
-  "AisBreadcrumb",
-  "AisClearRefinements",
-  "AisConfigure",
-  "AisCurrentRefinements",
-  "AisHierarchicalMenu",
-  "AisHighlight",
-  "AisHits",
-  "AisHitsPerPage",
-  "AisInstantSearch",
-  "AisInfiniteHits",
-  "AisMenu",
-  "AisMenuSelect",
-  "AisNumericMenu",
-  "AisPagination",
-  "AisPanel",
-  "AisPoweredBy",
-  "AisRangeInput",
-  "AisRatingMenu",
-  "AisRefinementList",
-  "AisSearchState",
-  "AisSearchBox",
-  "AisSnippet",
-  "AisSortBy",
-  "AisStats",
-  "AisToggleRefinement",
-]
-`);
-  });
-
-  it('should have `name` the same as the suit class name everywhere', () => {
-    const components = allInstalledComponents.map(
-      ([installedName, { name, mixins }]) => {
-        let suitClass = `Error! ${name} is missing the suit classes`;
-
-        try {
-          suitClass = mixins
-            .find(mixin => mixin.methods && mixin.methods.suit)
-            .methods.suit();
-        } catch (e) {
-          /* no suit class! */
-        }
-
-        return {
-          installedName,
-          name,
-          suitClass,
-        };
+      try {
+        suitClass = mixins
+          .find(mixin => mixin.methods && mixin.methods.suit)
+          .methods.suit();
+      } catch (e) {
+        /* no suit class! */
       }
-    );
 
-    components.forEach(({ name, installedName, suitClass }) => {
-      expect(installedName).toBe(name);
-      expect(suitClass).toBe(`ais-${name.substr(3)}`);
-    });
+      return {
+        installedName,
+        name,
+        suitClass,
+      };
+    }
+  );
+
+  components.forEach(({ name, installedName, suitClass }) => {
+    expect(installedName).toBe(name);
+    expect(suitClass).toBe(`ais-${name.substr(3)}`);
   });
 });
