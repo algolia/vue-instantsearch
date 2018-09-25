ffd81289bee1f672752a7c8cc7fbbb2e7d334544
Haroen Viaene <hello@haroen.me>
2018-09-25 10:45:35 +0200

    test(main): test install & suit naming is correct

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,13 +1,16 @@
+/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 import Vue from 'vue';
 import InstantSearch from '../instantsearch';
 
-test('Should register all components when installed', () => {
+describe('installing the plugin', () => {
   Vue.component = jest.fn();
   Vue.use(InstantSearch);
 
-  const allInstalledComponents = Vue.component.mock.calls.map(call => call[0]);
+  const allInstalledComponents = Vue.component.mock.calls;
 
-  expect(allInstalledComponents).toMatchInlineSnapshot(`
+  it('should register all components when installed', () => {
+    expect(allInstalledComponents.map(([name, _component]) => name))
+      .toMatchInlineSnapshot(`
 Array [
   "AisAutocomplete",
   "AisBreadcrumb",
@@ -37,4 +40,32 @@ Array [
   "AisToggleRefinement",
 ]
 `);
+  });
+
+  it('should have `name` the same as the suit class name everywhere', () => {
+    const components = allInstalledComponents.map(
+      ([installedName, { name, mixins }]) => {
+        let suitClass = `Error! ${name} is missing the suit classes`;
+
+        try {
+          suitClass = mixins
+            .find(mixin => mixin.methods && mixin.methods.suit)
+            .methods.suit();
+        } catch (e) {
+          /* no suit class! */
+        }
+
+        return {
+          installedName,
+          name,
+          suitClass,
+        };
+      }
+    );
+
+    components.forEach(({ name, installedName, suitClass }) => {
+      expect(installedName).toBe(name);
+      expect(suitClass).toBe(`ais-${name.substr(3)}`);
+    });
+  });
 });
