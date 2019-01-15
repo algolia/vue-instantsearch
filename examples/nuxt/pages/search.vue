c746fbb70fd6b0e0f79661847913c49721030fb6
Haroen Viaene <hello@haroen.me>
2019-01-15 11:37:56 +0100

    docs(nuxt): add example

---
+++ examples/nuxt/pages/search.vue
@@ -0,0 +1,99 @@
+<template>
+  <ais-instant-search-ssr>
+    <ais-search-box />
+    <ais-stats />
+    <ais-refinement-list attribute="genre" />
+    <ais-hits>
+      <template
+        slot="item"
+        slot-scope="{ item }"
+      >
+        <ais-highlight
+          attribute="title"
+          :hit="item"
+        />
+        <p class="year">{{ item.year }}</p>
+        <p class="genre">
+          <span
+            v-for="genre in item.genre"
+            :key="genre"
+            class="badge"
+          >
+            {{ genre }}
+          </span>
+        </p>
+      </template>
+    </ais-hits>
+    <ais-pagination />
+  </ais-instant-search-ssr>
+</template>
+
+<script>
+import {
+  AisInstantSearch,
+  AisInstantSearchSsr,
+  AisRefinementList,
+  AisHits,
+  AisHighlight,
+  AisSearchBox,
+  AisStats,
+  AisPagination,
+  createInstantSearch,
+} from '../../../src/instantsearch.js'; // TODO: move this to 'vue-instantsearch'
+import algoliasearch from 'algoliasearch/lite';
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+const { instantsearch, rootMixin } = createInstantSearch({
+  searchClient,
+  indexName: 'movies',
+});
+
+export default {
+  asyncData(context) {
+    return instantsearch.findResultsState({
+      query: 'hi',
+      hitsPerPage: 5,
+      disjunctiveFacets: ['genre'],
+      disjunctiveFacetsRefinements: { genre: ['Comedy'] },
+    });
+  },
+  beforeMount() {
+    instantsearch.hydrate(this.ais);
+  },
+  mixins: [rootMixin],
+  components: {
+    AisInstantSearch,
+    AisInstantSearchSsr,
+    AisRefinementList,
+    AisHits,
+    AisHighlight,
+    AisSearchBox,
+    AisStats,
+    AisPagination,
+  },
+  head() {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href:
+            'https://unpkg.com/instantsearch.css@7.1.0/themes/algolia-min.css',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.ais-Hits-list {
+  text-align: left;
+}
+.ais-InstantSearch {
+  margin: 1em;
+}
+</style>
