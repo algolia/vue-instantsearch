012a19879f04dc1bd4e64aa43fbbfb6cff69d3c7
Haroen Viaene <hello@haroen.me>
2019-01-18 10:03:46 +0100

    chore(examples): remove unused sample files

--- examples/nuxt/pages/index.vue
+++ examples/nuxt/pages/index.vue
@@ -1,27 +1,7 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt
-      </h1>
-      <h2 class="subtitle">
-        Vue InstantSearch &amp; Nuxt
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
-  </section>
+  <p>
+    Go to the search page: <nuxt-link to="/search">"/search"</nuxt-link>
+  </p>
 </template>
 
 <style>
--- examples/ssr/src/views/Home.vue
+++ examples/ssr/src/views/Home.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="home">
-    <img
-      alt="Vue logo"
-      src="../assets/logo.png"
-    >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    This is the home page.
   </div>
 </template>
 
