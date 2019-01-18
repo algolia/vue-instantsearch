858d256948f0b31e919b630496a935eb5c95a32f
Haroen Viaene <hello@haroen.me>
2019-01-18 14:58:54 +0100

    chore(nuxt): easier example

--- examples/nuxt/pages/index.vue
+++ examples/nuxt/pages/index.vue
@@ -1,37 +1,11 @@
 <template>
-  <p>
+  <h2>
     Go to the search page: <nuxt-link to="/search">"/search"</nuxt-link>
-  </p>
+  </h2>
 </template>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+h2 {
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
