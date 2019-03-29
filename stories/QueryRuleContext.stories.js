a2ec00179947424eb24e79e76e6a06473d231c5d
Haroen Viaene <hello@haroen.me>
2019-03-29 14:21:43 +0100

    docs(query-rules): add stories

--- stories/QueryRuleContext.stories.js
+++ stories/QueryRuleContext.stories.js
@@ -2,29 +2,135 @@ import { previewWrapper } from './utils';
 import { storiesOf } from '@storybook/vue';
 
 storiesOf('ais-query-rule-context', module)
-  .addDecorator(previewWrapper())
+  .addDecorator(
+    previewWrapper({
+      indexName: 'instant_search_movies',
+      filters: '<ais-refinement-list attribute="genre" />',
+      hits: `
+      <ol slot-scope="{ items }" class="playground-hits">
+        <li
+          v-for="item in items"
+          :key="item.objectID"
+          class="playground-hits-item"
+        >
+          <div
+            class="playground-hits-image"
+            :style="{ backgroundImage: 'url(' + item.image + ')' }"
+          />
+          <article>
+            <header>
+              <strong><ais-highlight attribute="title" :hit="item"/></strong>
+            </header>
+          </article>
+        </li>
+      </ol>
+      `,
+    })
+  )
   .add('simple usage', () => ({
-    template: `<ais-query-rule-context :tracked-filters="trackedFilters" />`,
+    template: `
+    <div>
+      <ul>
+        <li>Select the "Drama" category and The Shawshank Redemption appears</li>
+        <li>Select the "Thriller" category and Pulp Fiction appears</li>
+        <li>Type <q>music</q> and a banner will appear.</li>
+      </ul>
+      <ais-query-rule-context :tracked-filters="trackedFilters" />
+      <ais-query-rule-custom-data>
+        <template slot="item" slot-scope="{ item }">
+          <h2>{{ item.title }}</h2>
+          <a :href="item.link">
+            <img
+              :src="item.banner"
+              :alt="item.title"
+              :style="{ width: '100%' }"
+            />
+          </a>
+        </template>
+      </ais-query-rule-custom-data>
+    </div>`,
     data() {
       return {
         trackedFilters: {
-          brand: filters => filters,
+          genre: () => ['Thriller', 'Drama'],
         },
       };
     },
   }))
-  .add('static list with transform', () => ({
+  .add('with initial filter applied', () => ({
     template: `
-      <ais-query-rule-context
-        :tracked-filters="trackedFilters"
-        :transform-rule-contexts="transformRuleContexts"
-      />`,
+    <div>
+      <ul>
+        <li>Select the "Drama" category and The Shawshank Redemption appears</li>
+        <li>Select the "Thriller" category and Pulp Fiction appears</li>
+        <li>Type <q>music</q> and a banner will appear.</li>
+      </ul>
+      <ais-configure
+        :disjunctive-Facets-Refinements.camel="{
+          genre: ['Drama']
+        }"
+      />
+      <ais-query-rule-context :tracked-filters="trackedFilters" />
+      <ais-query-rule-custom-data>
+        <template slot="item" slot-scope="{ item }">
+          <h2>{{ item.title }}</h2>
+          <a :href="item.link">
+            <img
+              :src="item.banner"
+              :alt="item.title"
+              :style="{ width: '100%' }"
+            />
+          </a>
+        </template>
+      </ais-query-rule-custom-data>
+    </div>
+    `,
     data() {
       return {
         trackedFilters: {
-          brand: filters => filters,
+          genre: () => ['Thriller', 'Drama'],
         },
-        transformRuleContexts: () => ['i-like-ice-cream'],
+      };
+    },
+  }))
+  .add('with filter out "thriller" in generated rules', () => ({
+    template: `
+    <div>
+      <ul>
+        <li>Select the "Drama" category and The Shawshank Redemption appears</li>
+        <li>Select the "Thriller" category and Pulp Fiction does not appear</li>
+        <li>Type <q>music</q> and a banner will appear.</li>
+      </ul>
+      <ais-configure
+        :disjunctive-Facets-Refinements.camel="{
+          genre: ['Drama']
+        }"
+      />
+      <ais-query-rule-context
+        :tracked-filters="trackedFilters"
+        :transform-rule-contexts="transformRuleContexts"
+      />
+      <ais-query-rule-custom-data>
+        <template slot="item" slot-scope="{ item }">
+          <h2>{{ item.title }}</h2>
+          <a :href="item.link">
+            <img
+              :src="item.banner"
+              :alt="item.title"
+              :style="{ width: '100%' }"
+            />
+          </a>
+        </template>
+      </ais-query-rule-custom-data>
+    </div>
+    `,
+    data() {
+      return {
+        trackedFilters: {
+          genre: values => values,
+        },
+        transformRuleContexts: rules =>
+          rules.filter(rule => rule.indexOf('Thriller') < 0),
       };
     },
   }));
--- stories/QueryRuleCustomData.stories.js
+++ stories/QueryRuleCustomData.stories.js
@@ -2,18 +2,88 @@ import { previewWrapper } from './utils';
 import { storiesOf } from '@storybook/vue';
 
 storiesOf('ais-query-rule-custom-data', module)
-  .addDecorator(previewWrapper())
+  .addDecorator(
+    previewWrapper({
+      indexName: 'instant_search_movies',
+      filters: '<ais-refinement-list attribute="genre" />',
+      hits: `
+      <ol slot-scope="{ items }" class="playground-hits">
+        <li
+          v-for="item in items"
+          :key="item.objectID"
+          class="playground-hits-item"
+        >
+          <div
+            class="playground-hits-image"
+            :style="{ backgroundImage: 'url(' + item.image + ')' }"
+          />
+          <article>
+            <header>
+              <strong><ais-highlight attribute="title" :hit="item"/></strong>
+            </header>
+          </article>
+        </li>
+      </ol>
+      `,
+    })
+  )
   .add('simple usage', () => ({
-    template: `<ais-query-rule-custom-data/>`,
-  }))
-  .add('static list with transform', () => ({
     template: `
-      <ais-query-rule-custom-data
-        :transform-items="transformItems"
-      />`,
+    <div>
+      <p>
+        Type <q>music</q> and a banner will appear.
+      </p>
+      <ais-query-rule-custom-data>
+        <template slot="item" slot-scope="{ item }">
+          <h2>{{ item.title }}</h2>
+          <a :href="item.link">
+            <img
+              :src="item.banner"
+              :alt="item.title"
+              :style="{ width: '100%' }"
+            />
+          </a>
+        </template>
+      </ais-query-rule-custom-data>
+    </div>
+    `,
+  }))
+  .add('with default banner', () => ({
+    template: `
+    <div>
+      <p>
+        Kill Bill appears whenever no other results are promoted. Type
+        <q>music</q> to see another movie promoted.
+      </p>
+      <ais-query-rule-custom-data :transform-items="transformItems">
+        <template slot="item" slot-scope="{ item }">
+          <h2>{{ item.title }}</h2>
+          <a :href="item.link">
+            <img
+              :src="item.banner"
+              :alt="item.title"
+              :style="{ width: '100%' }"
+            />
+          </a>
+        </template>
+      </ais-query-rule-custom-data>
+    </div>
+    `,
     data() {
       return {
-        transformItems: () => [{ message: 'Hello World!' }],
+        transformItems: items => {
+          if (items.length > 0) {
+            return [items[0]];
+          }
+
+          return [
+            {
+              title: 'Kill Bill',
+              banner: 'http://static.bobatv.net/IMovie/mv_2352/poster_2352.jpg',
+              link: 'https://www.netflix.com/title/60031236',
+            },
+          ];
+        },
       };
     },
   }))
@@ -25,6 +95,18 @@ storiesOf('ais-query-rule-custom-data', module)
     data() {
       return {
         transformItems: items => [items[0]],
+      };
+    },
+  }))
+  .add('keeping only banners with transform', () => ({
+    template: `
+      <ais-query-rule-custom-data
+        :transform-items="transformItems"
+      />`,
+    data() {
+      return {
+        transformItems: items =>
+          items.filter(item => item.banner !== undefined),
       };
     },
   }));
