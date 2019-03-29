34ac189460debce83dfb78de950f15d568526c4c
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2019-03-29 19:46:52 -0300

    Revert "Improve InstantSearchSsr to allow multi indexes by setting index name prop"
    This reverts commit 13e8d99d

--- src/components/InstantSearchSsr.js
+++ src/components/InstantSearchSsr.js
@@ -2,20 +2,11 @@ import { createInstantSearchComponent } from '../util/createInstantSearchCompone
 
 export default createInstantSearchComponent({
   name: 'AisInstantSearchSsr',
-  props: {
-    indexName: {
-      type: String,
-      required: true,
-    },
-  },
   inject: {
     // should be possible to configure this with {camelcase: ['error', {allow: ['^\\$_']}]}
     // but that didn't work
     // eslint-disable-next-line camelcase
     $_ais: {
-      from() {
-        return `$_ais-${this.indexName}`;
-      },
       default() {
         throw new Error('`rootMixin` is required when using SSR.');
       },
