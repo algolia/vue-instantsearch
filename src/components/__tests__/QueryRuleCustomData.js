a78932d7ce236676d078e5c68d675ac8dd5573fb
Haroen Viaene <hello@haroen.me>
2019-04-29 17:09:58 +0200

    test(query-rule): add slot tests

--- src/components/__tests__/QueryRuleCustomData.js
+++ src/components/__tests__/QueryRuleCustomData.js
@@ -7,7 +7,9 @@ it('renders in a list of <pre> by default', () => {
   __setState({
     items: [{ text: 'this is user data' }, { text: 'this too!' }],
   });
+
   const wrapper = mount(QueryRuleCustomData);
+
   expect(wrapper.html()).toMatchInlineSnapshot(`
 
 <div class="ais-QueryRuleCustomData">
@@ -28,6 +30,41 @@ it('renders in a list of <pre> by default', () => {
 </div>
 
 `);
+});
+
+it('gives the items to the main slot', () => {
+  const items = [{ text: 'this is user data' }, { text: 'this too!' }];
+  __setState({
+    items,
+  });
+
+  mount(QueryRuleCustomData, {
+    scopedSlots: {
+      default(props) {
+        expect(props).toEqual({
+          items,
+        });
+      },
+    },
+  });
+});
+
+it('gives individual items to the item slot', () => {
+  const items = [{ text: 'this is user data' }, { text: 'this too!' }];
+  expect.assertions(items.length);
+  __setState({
+    items,
+  });
+
+  mount(QueryRuleCustomData, {
+    scopedSlots: {
+      item(props) {
+        expect(props).toEqual({
+          item: expect.objectContaining({ text: expect.any(String) }),
+        });
+      },
+    },
+  });
 });
 
 it('accepts transformItems', () => {
