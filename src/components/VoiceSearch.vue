dbaec7b9fff3f97bc3074cce0ce5138fbc06cf8b
Eunjae Lee <eunjae.lee@algolia.com>
2019-04-29 16:45:02 +0200

    chore(voiceSearch): fix lint errors

--- src/components/VoiceSearch.vue
+++ src/components/VoiceSearch.vue
@@ -1,5 +1,8 @@
 <template>
-  <div v-if="state" :class="suit()">
+  <div
+    v-if="state"
+    :class="suit()"
+  >
     <slot>
       <button
         type="button"
@@ -8,28 +11,65 @@
         :disabled="!isBrowserSupported"
         @click="handleClick"
       >
-        <slot name="buttonText" v-bind="innerSlotProps">
-          <svg v-bind="buttonSvgAttrs" v-if="errorNotAllowed">
-            <line x1="1" y1="1" x2="23" y2="23"></line>
-            <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6"></path>
-            <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23"></path>
-            <line x1="12" y1="19" x2="12" y2="23"></line>
-            <line x1="8" y1="23" x2="16" y2="23"></line>
+        <slot
+          name="buttonText"
+          v-bind="innerSlotProps"
+        >
+          <svg
+            v-bind="buttonSvgAttrs"
+            v-if="errorNotAllowed"
+          >
+            <line
+              x1="1"
+              y1="1"
+              x2="23"
+              y2="23"
+            />
+            <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6" />
+            <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23" />
+            <line
+              x1="12"
+              y1="19"
+              x2="12"
+              y2="23"
+            />
+            <line
+              x1="8"
+              y1="23"
+              x2="16"
+              y2="23"
+            />
           </svg>
-          <svg v-bind="buttonSvgAttrs" v-else>
+          <svg
+            v-bind="buttonSvgAttrs"
+            v-else
+          >
             <path
               d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"
               :fill="isListening ? 'currentColor' : 'none'"
-            ></path>
-            <path d="M19 10v2a7 7 0 0 1-14 0v-2"></path>
-            <line x1="12" y1="19" x2="12" y2="23"></line>
-            <line x1="8" y1="23" x2="16" y2="23"></line>
+            />
+            <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
+            <line
+              x1="12"
+              y1="19"
+              x2="12"
+              y2="23"
+            />
+            <line
+              x1="8"
+              y1="23"
+              x2="16"
+              y2="23"
+            />
           </svg>
         </slot>
       </button>
       <div :class="suit('status')">
-        <slot name="status" v-bind="innerSlotProps">
-          <p>{{transcript}}</p>
+        <slot
+          name="status"
+          v-bind="innerSlotProps"
+        >
+          <p>{{ transcript }}</p>
         </slot>
       </div>
     </slot>
