8a27c024011e4b343ea472bc14d2e22798cda4ac
Haroen Viaene <hello@haroen.me>
2020-05-13 12:07:27 +0200

    test

--- src/components/__tests__/Index-integration.js
+++ src/components/__tests__/Index-integration.js
@@ -2,7 +2,6 @@ jest.unmock('instantsearch.js/es');
 import { mount } from '@vue/test-utils';
 import Index from '../Index';
 import instantsearch from 'instantsearch.js/es';
-import { SearchParameters } from 'algoliasearch-helper';
 import { createWidgetMixin } from '../../mixins/widget';
 import { createFakeClient } from '../../util/testutils/client';
 
