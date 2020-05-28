423b284b641bc4c5e489c6995403792240b241a3
Hugo Alliaume <kocal@live.fr>
2020-05-28 13:02:34 +0200

    chore: add story

--- stories/ToggleRefinement.stories.js
+++ stories/ToggleRefinement.stories.js
@@ -20,6 +20,15 @@ storiesOf('ais-toggle-refinement', module)
       />
     `,
   }))
+  .add('with an on value (with multiple values)', () => ({
+    template: `
+      <ais-toggle-refinement
+        attribute="brand"
+        label="Brands starting with \"i\""
+        :on="['Samsung', 'Metra']"
+      />
+    `,
+  }))
   .add('with an off value', () => ({
     template: `
       <ais-toggle-refinement
