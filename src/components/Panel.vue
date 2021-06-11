2f681d14458f088ca7c7a7c03a0427990a03ad53
eunjae-lee <karis612@gmail.com>
2021-06-11 16:09:01 +0200

    fix(vue 3): fix Panel

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="[suit(), !canRefine && suit('', 'noRefinement')]">
     <div
-      v-if="$slots.header || $scopedSlots.header"
+      v-if="headerSlot"
       :class="suit('header')"
     >
       <slot
@@ -13,7 +13,7 @@
       <slot :has-refinements="canRefine" />
     </div>
     <div
-      v-if="$slots.footer || $scopedSlots.footer"
+      v-if="footerSlot"
       :class="suit('footer')"
     >
       <slot
@@ -25,11 +25,25 @@
 </template>
 
 <script>
+import { isVue3 } from 'vue-demi';
 import { createPanelProviderMixin } from '../mixins/panel';
 import { createSuitMixin } from '../mixins/suit';
 
 export default {
   name: 'AisPanel',
   mixins: [createSuitMixin({ name: 'Panel' }), createPanelProviderMixin()],
+  computed: {
+    headerSlot() {
+      // $scopedSlots doesn't exist in Vue3
+      return isVue3
+        ? this.$slots.header
+        : this.$slots.header || this.$scopedSlots.header;
+    },
+    footerSlot() {
+      return isVue3
+        ? this.$slots.footer
+        : this.$slots.footer || this.$scopedSlots.footer;
+    },
+  },
 };
 </script>
