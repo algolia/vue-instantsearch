ec5e6daa28df3e80f9e677144f59f73478f2d872
eunjae-lee <karis612@gmail.com>
2021-06-15 15:29:18 +0200

    fix: enable unit tests on vue3

--- src/mixins/__tests__/widget.test.js
+++ src/mixins/__tests__/widget.test.js
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils';
+import { mount } from '../../../test/utils';
 import { createWidgetMixin } from '../widget';
 
 const createFakeComponent = () => ({
--- test/utils/index.js
+++ test/utils/index.js
@@ -1,14 +1,18 @@
 import { isVue3 } from 'vue-demi';
-import { mount as mountVue2 } from '@vue/test-utils';
 
 export const mount = isVue3
   ? (component, options = {}) => {
-      const { propsData, ...restOptions } = options;
+      const { propsData, mixins, ...restOptions } = options;
       // If we `import` this, it will try to import Vue3-only APIs like `defineComponent`,
       // and jest will fail. So we need to `require` it.
-      return require('@vue/test-utils2').mount(component, {
+      const wrapper = require('@vue/test-utils2').mount(component, {
         ...restOptions,
         props: propsData,
+        global: {
+          mixins,
+        },
       });
+      wrapper.destroy = () => wrapper.unmount();
+      return wrapper;
     }
-  : mountVue2;
+  : require('@vue/test-utils').mount;
