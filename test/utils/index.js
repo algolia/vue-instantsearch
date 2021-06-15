3c38d4a56ca88df1f9103fba328db3b4b42f14a7
eunjae-lee <karis612@gmail.com>
2021-06-15 16:07:46 +0200

    fix: update wrapper for `provide`

--- test/utils/index.js
+++ test/utils/index.js
@@ -2,7 +2,7 @@ import { isVue3 } from 'vue-demi';
 
 export const mount = isVue3
   ? (component, options = {}) => {
-      const { propsData, mixins, ...restOptions } = options;
+      const { propsData, mixins, provide, ...restOptions } = options;
       // If we `import` this, it will try to import Vue3-only APIs like `defineComponent`,
       // and jest will fail. So we need to `require` it.
       const wrapper = require('@vue/test-utils2').mount(component, {
@@ -10,6 +10,7 @@ export const mount = isVue3
         props: propsData,
         global: {
           mixins,
+          provide,
         },
       });
       wrapper.destroy = () => wrapper.unmount();
