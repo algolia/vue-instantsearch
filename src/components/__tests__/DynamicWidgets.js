2002f4b5fb8fa893193b4eaf7bc30a1083206fb4
Haroen Viaene <hello@haroen.me>
2021-06-16 17:23:16 +0200

    write more tests

--- src/components/__tests__/DynamicWidgets.js
+++ src/components/__tests__/DynamicWidgets.js
@@ -4,6 +4,39 @@ import { __setState } from '../../mixins/widget';
 import { plugin } from '../../plugin';
 jest.mock('../../mixins/widget');
 
+const MockRefinementList = {
+  props: { attribute: { type: String } },
+  render(h) {
+    return h('div', {
+      attrs: {
+        'widget-name': 'ais-refinement-list',
+        attribute: this.attribute,
+      },
+    });
+  },
+};
+
+const MockMenu = {
+  props: { attribute: { type: String } },
+  render(h) {
+    return h('div', {
+      attrs: { 'widget-name': 'ais-menu', attribute: this.attribute },
+    });
+  },
+};
+
+const MockHierarchicalMenu = {
+  props: { attributes: { type: Array } },
+  render(h) {
+    return h('div', {
+      attrs: {
+        'widget-name': 'ais-hierarchical-menu',
+        attributes: this.attributes,
+      },
+    });
+  },
+};
+
 it('renders all children without state', () => {
   const localVue = createLocalVue();
 
@@ -19,27 +52,43 @@ it('renders all children without state', () => {
     slots: {
       default: `
       <ais-refinement-list attribute="test1"/>
-      <ais-refinement-list attribute="test2"/>
+      <ais-menu attribute="test2"/>
       <ais-panel>
-        <ais-hierarchical-menu :attributes="['test3', 'test3']" />
+        <ais-hierarchical-menu :attributes="['test3', 'test4']" />
       </ais-panel>
       `,
     },
+    stubs: {
+      'ais-refinement-list': MockRefinementList,
+      'ais-menu': MockMenu,
+      'ais-hierarchical-menu': MockHierarchicalMenu,
+    },
   });
 
-  // the inner widgets don't render anything because state is falsy, but they are mounted
   expect(wrapper.html()).toMatchInlineSnapshot(`
 
 <div hidden="hidden"
      class="ais-DynamicWidgets"
 >
   <div class="ais-DynamicWidgets-widget">
+    <div widget-name="ais-refinement-list"
+         attribute="test1"
+    >
+    </div>
   </div>
   <div class="ais-DynamicWidgets-widget">
+    <div widget-name="ais-menu"
+         attribute="test2"
+    >
+    </div>
   </div>
   <div class="ais-DynamicWidgets-widget">
     <div class="ais-Panel">
       <div class="ais-Panel-body">
+        <div widget-name="ais-hierarchical-menu"
+             attributes="test3,test4"
+        >
+        </div>
       </div>
     </div>
   </div>
@@ -73,7 +122,6 @@ it('renders nothing with empty items', () => {
 
   __setState({
     attributesToRender: [],
-    items: [],
   });
 
   const wrapper = mount(DynamicWidgets, {
@@ -84,6 +132,10 @@ it('renders nothing with empty items', () => {
     slots: {
       default: `<ais-refinement-list attribute="test1"/>`,
     },
+    stubs: {
+      'ais-refinement-list': MockRefinementList,
+      'ais-menu': MockMenu,
+    },
   });
 
   expect(wrapper.html()).toMatchInlineSnapshot(`
@@ -94,21 +146,13 @@ it('renders nothing with empty items', () => {
 `);
 });
 
-it('renders attributesToRender 1', () => {
+it('renders attributesToRender (menu)', () => {
   const localVue = createLocalVue();
 
   localVue.use(plugin);
 
   __setState({
     attributesToRender: ['test1'],
-    items: [],
-  });
-
-  localVue.component('test-component', {
-    props: { attribute: { type: String } },
-    render(h) {
-      return h('div', {}, this.attribute);
-    },
   });
 
   const wrapper = mount(DynamicWidgets, {
@@ -118,9 +162,13 @@ it('renders attributesToRender 1', () => {
     },
     slots: {
       default: `
-        <test-component attribute="test1" />
+        <ais-menu attribute="test1" />
         <ais-refinement-list attribute="test2" />
       `,
+    },
+    stubs: {
+      'ais-refinement-list': MockRefinementList,
+      'ais-menu': MockMenu,
     },
   });
 
@@ -128,8 +176,9 @@ it('renders attributesToRender 1', () => {
 
 <div class="ais-DynamicWidgets">
   <div class="ais-DynamicWidgets-widget">
-    <div>
-      test1
+    <div widget-name="ais-menu"
+         attribute="test1"
+    >
     </div>
   </div>
 </div>
@@ -137,20 +186,146 @@ it('renders attributesToRender 1', () => {
 `);
 });
 
-it('renders attributesToRender 2', () => {
+it('renders attributesToRender (refinement list)', () => {
   const localVue = createLocalVue();
 
   localVue.use(plugin);
 
   __setState({
     attributesToRender: ['test2'],
-    items: [],
   });
 
-  localVue.component('test-component', {
-    props: { attribute: { type: String } },
-    render(h) {
-      return h('div', {}, this.attribute);
+  const wrapper = mount(DynamicWidgets, {
+    localVue,
+    propsData: {
+      transformItems: items => items,
+    },
+    slots: {
+      default: `
+        <ais-menu attribute="test1" />
+        <ais-refinement-list attribute="test2" />
+      `,
+    },
+    stubs: {
+      'ais-refinement-list': MockRefinementList,
+      'ais-menu': MockMenu,
+    },
+  });
+
+  expect(wrapper.html()).toMatchInlineSnapshot(`
+
+<div class="ais-DynamicWidgets">
+  <div class="ais-DynamicWidgets-widget">
+    <div widget-name="ais-refinement-list"
+         attribute="test2"
+    >
+    </div>
+  </div>
+</div>
+
+`);
+});
+
+it('renders attributesToRender (panel)', () => {
+  const localVue = createLocalVue();
+
+  localVue.use(plugin);
+
+  __setState({
+    attributesToRender: ['test2'],
+  });
+
+  const wrapper = mount(DynamicWidgets, {
+    localVue,
+    propsData: {
+      transformItems: items => items,
+    },
+    slots: {
+      default: `
+        <ais-menu attribute="test1" />
+        <ais-panel>
+          <ais-refinement-list attribute="test2" />
+        </ais-panel>
+      `,
+    },
+    stubs: {
+      'ais-refinement-list': MockRefinementList,
+      'ais-menu': MockMenu,
+    },
+  });
+
+  expect(wrapper.html()).toMatchInlineSnapshot(`
+
+<div class="ais-DynamicWidgets">
+  <div class="ais-DynamicWidgets-widget">
+    <div class="ais-Panel">
+      <div class="ais-Panel-body">
+        <div widget-name="ais-refinement-list"
+             attribute="test2"
+        >
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+`);
+});
+
+it('renders attributesToRender (hierarchical menu)', () => {
+  const localVue = createLocalVue();
+
+  localVue.use(plugin);
+
+  __setState({
+    attributesToRender: ['test1'],
+  });
+
+  const wrapper = mount(DynamicWidgets, {
+    localVue,
+    propsData: {
+      transformItems: items => items,
+    },
+    slots: {
+      default: `
+        <ais-hierarchical-menu :attributes="['test1','test2']" />
+        <ais-menu attribute="test3" />
+        <ais-panel>
+          <ais-refinement-list attribute="test4" />
+        </ais-panel>
+      `,
+    },
+    stubs: {
+      'ais-refinement-list': MockRefinementList,
+      'ais-menu': MockMenu,
+      'ais-hierarchical-menu': MockHierarchicalMenu,
+    },
+  });
+
+  expect(wrapper.html()).toMatchInlineSnapshot(`
+
+<div class="ais-DynamicWidgets">
+  <div class="ais-DynamicWidgets-widget">
+    <div widget-name="ais-hierarchical-menu"
+         attributes="test1,test2"
+    >
+    </div>
+  </div>
+</div>
+
+`);
+});
+
+it('updates DOM when attributesToRender changes', () => {
+  const localVue = createLocalVue();
+
+  localVue.use(plugin);
+
+  let attributesToRender = ['test1'];
+
+  __setState({
+    get attributesToRender() {
+      return attributesToRender;
     },
   });
 
@@ -161,9 +336,17 @@ it('renders attributesToRender 2', () => {
     },
     slots: {
       default: `
-        <test-component attribute="test1" />
-        <ais-refinement-list attribute="test2" />
+        <ais-hierarchical-menu :attributes="['test1','test2']" />
+        <ais-menu attribute="test3" />
+        <ais-panel>
+          <ais-refinement-list attribute="test4" />
+        </ais-panel>
       `,
+    },
+    stubs: {
+      'ais-refinement-list': MockRefinementList,
+      'ais-menu': MockMenu,
+      'ais-hierarchical-menu': MockHierarchicalMenu,
     },
   });
 
@@ -171,6 +354,54 @@ it('renders attributesToRender 2', () => {
 
 <div class="ais-DynamicWidgets">
   <div class="ais-DynamicWidgets-widget">
+    <div widget-name="ais-hierarchical-menu"
+         attributes="test1,test2"
+    >
+    </div>
+  </div>
+</div>
+
+`);
+
+  attributesToRender = ['test3'];
+
+  wrapper.vm.$forceUpdate();
+
+  expect(wrapper.html()).toMatchInlineSnapshot(`
+
+<div class="ais-DynamicWidgets">
+  <div class="ais-DynamicWidgets-widget">
+    <div widget-name="ais-menu"
+         attribute="test3"
+    >
+    </div>
+  </div>
+</div>
+
+`);
+
+  attributesToRender = ['test1', 'test4'];
+
+  wrapper.vm.$forceUpdate();
+
+  expect(wrapper.html()).toMatchInlineSnapshot(`
+
+<div class="ais-DynamicWidgets">
+  <div class="ais-DynamicWidgets-widget">
+    <div widget-name="ais-hierarchical-menu"
+         attributes="test1,test2"
+    >
+    </div>
+  </div>
+  <div class="ais-DynamicWidgets-widget">
+    <div class="ais-Panel">
+      <div class="ais-Panel-body">
+        <div widget-name="ais-refinement-list"
+             attribute="test4"
+        >
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
