b497bf432bebf476366536d5c7cc54d1005e957d
eunjae-lee <karis612@gmail.com>
2021-07-15 13:50:32 +0200

    chore: remove unused file

--- test/jest-serializer-html.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-commonjs
-const createSerializer = require('jest-serializer-html/createSerializer');
-
-// eslint-disable-next-line import/no-commonjs
-module.exports = createSerializer({
-  print: {
-    sortAttributes: names => names.sort(),
-  },
-});
