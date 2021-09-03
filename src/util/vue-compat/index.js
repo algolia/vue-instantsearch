6212389d87f54b6d90477efee16a9d27bcdb8a65
eunjae-lee <karis612@gmail.com>
2021-09-03 18:04:52 +0200

    update comment

--- src/util/vue-compat/index.js
+++ src/util/vue-compat/index.js
@@ -1,1 +1,7 @@
+/*
+  By default, we maintain this repository based on Vue 2.
+  That's why this file is exporting from `index-2`,
+  which includes all the variables and methods for Vue 2.
+  When it's built, rollup will replace it with either `index-2.js` or `index-3.js`.
+*/
 export * from './index-2';
