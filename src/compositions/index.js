c9a8f94e4bcdb447a82227986900646f1a97967d
Haroen Viaene <hello@haroen.me>
2021-10-21 13:38:02 +0100

    correct exports

--- src/compositions/index.js
+++ src/compositions/index.js
@@ -1,2 +1,6 @@
 export { useConnector as EXPERIMENTAL_useConnector } from './useConnector';
 export { useSearchBox as EXPERIMENTAL_useSearchBox } from './useSearchBox';
+export { useConfigure as EXPERIMENTAL_useConfigure } from './useConfigure';
+export {
+  useRefinementList as EXPERIMENTAL_useRefinementList,
+} from './useRefinementList';
