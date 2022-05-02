740e4c88b0b39e1a1b06421b622528f8eebc8067
Haroen Viaene <hello@haroen.me>
2022-05-02 17:46:51 +0200

    feat(widgets): pass $$widgetType
    This is used for metadata, and allows to distinguish between builtin widgets and custom ones using `createWidgetMixin` by the users.
    
    The design using `additionalParameters` is inspired by what we chose in React InstantSearch Hooks

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,9 +1,18 @@
-import { isVue3, Vue2, createApp } from '../util/vue-compat';
+import { isVue3, Vue2, createApp, Vue, renderCompat } from '../util/vue-compat';
 import InstantSearch from '../instantsearch';
+import { AisInstantSearch } from '../widgets';
 
 const renderlessComponents = ['AisExperimentalConfigureRelatedItems'];
+const nonWidgetComponents = [
+  'AisInstantSearch',
+  'AisInstantSearchSsr',
+  'AisHighlight',
+  'AisSnippet',
+  'AisPanel',
+  'AisPoweredBy',
+];
 
-it('should have `name` the same as the suit class name everywhere', () => {
+function getAllComponents() {
   let calls;
   if (isVue3) {
     const app = createApp();
@@ -16,37 +25,105 @@ it('should have `name` the same as the suit class name everywhere', () => {
     calls = Vue2.component.mock.calls;
   }
 
-  const allInstalledComponents = calls.filter(
-    ([installedName]) => !renderlessComponents.includes(installedName)
-  );
-  const components = allInstalledComponents.map(
-    ([installedName, { name, mixins }]) => {
-      let suitClass = `Error! ${name} is missing the suit classes`;
+  return calls.map(([installedName, call]) => {
+    const { name, mixins } = call;
+    let suitClass = `Error! ${name} is missing the suit classes`;
+    let widget = `Error! ${name} is missing the widget`;
 
-      try {
-        suitClass = mixins
-          .find(mixin => mixin.methods && mixin.methods.suit)
-          .methods.suit();
-      } catch (e) {
-        /* no suit class, so will fail the assertions */
+    try {
+      suitClass = mixins
+        .find(mixin => mixin.methods && mixin.methods.suit)
+        .methods.suit();
+    } catch (e) {
+      /* no suit class, so will fail the assertions */
+    }
+
+    try {
+      if (nonWidgetComponents.includes(name)) {
+        throw new Error('not a widget');
       }
+      const vm = new Vue({
+        render: renderCompat(h =>
+          h(
+            AisInstantSearch,
+            {
+              props: {
+                indexName: 'instant_search',
+                searchClient: {
+                  search() {
+                    return new Promise({ results: [] });
+                  },
+                },
+              },
+            },
+            [
+              h(call, {
+                props: {
+                  indexName: 'index',
+                  attribute: 'attr',
+                  attributes: ['attr'],
+                  hit: {},
+                  label: '',
+                  items: [{ default: true, value: 1, label: 'one' }],
+                  trackedFilters: {},
+                  matchingPatterns: {},
+                },
+                ref: 'widgetComponent',
+              }),
+            ]
+          )
+        ),
+      }).$mount();
+      widget = vm.$refs.widgetComponent.widget;
+    } catch (e) {
+      /* no widget, so will fail the assertions */
+    }
 
-      return {
-        installedName,
-        name,
-        suitClass,
-      };
+    return {
+      installedName,
+      name,
+      suitClass,
+      widget,
+    };
+  });
+}
+
+const components = getAllComponents();
+
+describe('DOM component', () => {
+  test.each(
+    components.filter(
+      ({ name }) => renderlessComponents.includes(name) === false
+    )
+  )(
+    '$name should have the same `name` as suit class',
+    ({ name, installedName, suitClass }) => {
+      expect(installedName).toBe(name);
+      if (name === 'AisInstantSearchSsr') {
+        expect(suitClass).toBe(`ais-InstantSearch`);
+      } else if (name === 'AisExperimentalDynamicWidgets') {
+        expect(suitClass).toBe(`ais-DynamicWidgets`);
+      } else {
+        expect(suitClass).toBe(`ais-${name.substr(3)}`);
+      }
     }
   );
+});
 
-  components.forEach(({ name, installedName, suitClass }) => {
-    expect(installedName).toBe(name);
-    if (name === 'AisInstantSearchSsr') {
-      expect(suitClass).toBe(`ais-InstantSearch`);
-    } else if (name === 'AisExperimentalDynamicWidgets') {
-      expect(suitClass).toBe(`ais-DynamicWidgets`);
+describe('installed widget', () => {
+  test.each(
+    components.filter(
+      ({ name }) => nonWidgetComponents.includes(name) === false
+    )
+  )('sets widgetType $name', ({ name, widget }) => {
+    if (name === 'AisExperimentalDynamicWidgets') {
+      expect(widget.$$widgetType).toBe('ais.dynamicWidgets');
+    } else if (name === 'AisExperimentalConfigureRelatedItems') {
+      expect(widget.$$widgetType).toBe('ais.configureRelatedItems');
     } else {
-      expect(suitClass).toBe(`ais-${name.substr(3)}`);
+      expect(widget.$$widgetType).toBe(
+        `ais.${name[3].toLowerCase()}${name.substr(4)}`
+      );
     }
   });
 });
